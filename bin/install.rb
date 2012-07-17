eab9ac9670d6beab2efd792b70cff20e998dc71a
MEHColeman <m@rkcoleman.co.uk>
2012-07-17 16:55:26 +0200

    Ignore README.md

--- bin/install.rb
+++ bin/install.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 require 'fileutils'
-SKIP_PATTERNS=[/^\./, /^bin$/, /^lib$/, /^util$/]
+SKIP_PATTERNS=[/^\./, /^bin$/, /^lib$/, /^util$/, /^README.md$/]
 install_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 home = ENV['HOME']
 Dir.new(install_dir).each do |filename|
