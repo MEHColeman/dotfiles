8f5e131cb1bca12fb9135221dab281c2f1545496
Mark Coleman <m@rkcoleman.co.uk>
2019-07-05 19:23:46 +0100

    Rename function to clarify intent

--- bin/install.rb
+++ bin/install.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 SKIP_PATTERNS=[/^\./, /^bin$/, /^lib$/, /^util$/, /^README.md$/, /^BASH_PROFILE_NOTES.txt$/, /^new_install.txt$/, /^NO_DOT_.*$/]
 install_from_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
-def op_is_ok?(filename, home_filename)
+def operation_is_ok?(filename, home_filename)
   !File.exists?(home_filename) &&
     !skip?(filename)
 end
@@ -19,7 +19,7 @@ Dir.new(install_from_dir).each do |filename|
  home_filename =  File.join(home, ".#{filename}")
  dotfile_filename = File.join(install_from_dir, filename)
 
- if op_is_ok?(filename, home_filename)
+ if operation_is_ok?(filename, home_filename)
   puts "will remove: home_filename = #{home_filename}"
   puts "dotfile_filename = #{dotfile_filename}"
   FileUtils.remove_entry home_filename, true
