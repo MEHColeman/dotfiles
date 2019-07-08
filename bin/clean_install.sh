b735c54f4598e3ceb1e1d4d3b13bd3ba047dfd6e
Mark Coleman <m@rkcoleman.co.uk>
2019-07-08 11:55:06 +0100

    Install oh-my-zsh script
    Add oh-my-zsh to the clean_install.sh script. Fix the fact that
    oh-my-zsh insists on blatting over existing .zshrc file.

--- bin/clean_install.sh
+++ bin/clean_install.sh
@@ -4,6 +4,6 @@ read -p "Are you certain you want to do this? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh --unattended
+  curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash -s -- --unattended
+  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 fi
