3b5af696da4a62a78efb5d7da8ce3b6db4d6df96
Mark Coleman <m@rkcoleman.co.uk>
2019-07-11 17:33:01 +0100

    Add more confirmations to install script

--- bin/clean_install.sh
+++ bin/clean_install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-read -p "Are you certain you want to do this? " -n 1 -r
-echo    # (optional) move to a new line
+echo "This script installs oh-my-zsh, tmuxifier, rbenv, and updates z.sh"
+echo "and any other .dotfile git submodules"
+read -p "Are you certain you want to do this? (y/n) " -n 1 -r
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash -s -- --unattended
-  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+  read -p "Install oh-my-zsh? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash -s -- --unattended
+    mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+  fi
 
-  git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+  read -p "Install tmuxifier? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+  fi
 
-  cd ~/.dotfiles
-  git submodule update --init --recursive
+  read -p "Install rbenv? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+  fi
+
+  read -p "Update .dotfile submodules? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    cd ~/.dotfiles
+    git submodule update --init --recursive
+  fi
 fi
