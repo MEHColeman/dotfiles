3efa24f287ae34c39a55ec31675fe7f441b910af
Mark Coleman <m@rkcoleman.co.uk>
2019-07-12 22:51:52 +0100

    Add common directories and projects

--- bin/dev_install.sh
+++ bin/dev_install.sh
@@ -5,6 +5,13 @@ read -p "Are you certain you want to do this? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  mkdir -p ~/dev/bw/projects/
+  mkdir -p ~/notes/
+  git clone git@github.com:MEHColeman/just_dev_things.git ~/notes/general
+  git clone git@github.com:MEHColeman/mehcoleman.github.io.git ~/notes/blog
+  cd ~/notes/blog || exit
+  git remote add private git@bitbucket.org:MEHColeman/blog.git
+
   vim +BundleInstall +qall
   cd ~/.vim/bundle/YouCompleteMe || exit
   ./install.py --all
