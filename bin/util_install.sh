5478e78e8e6c656e9d60c447c45171b7d1071209
Mark Coleman <m@rkcoleman.co.uk>
2019-11-20 22:42:00 +0000

    Create script to install useful packages
    Installl various useful and commonly used packages. Needs a bit more
    work on the linux side, but will attempt to brew install or apt-get
    install a bunch of packages.

---
+++ bin/util_install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "This script installs various useful packages on both linux and macOS"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+  Linux*)     machine=Linux;;
+  Darwin*)    machine=Mac;;
+  CYGWIN*)    machine=Cygwin;;
+  MINGW*)     machine=MinGw;;
+  *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+case $machine in
+  Mac)        install_command='brew install';;
+  Linux)      install_command='apt-get install -yqq';;
+  *)          install_commmand='#'
+esac
+
+echo $(install_command)
+
+# both brew and apt-get have the same name
+declare -a packages=(
+  "ack"
+  "bash"
+  "cmake"
+  "coreutils"
+  "dfu-util"
+  "elixir"
+  "ffmpeg"
+  "findutils"
+  "gcc"
+  "git"
+  "grep"
+  "hadolint"
+  "hub"
+  "kubernetes-cli"
+  "mosh"
+  "postgresql"
+  "rbenv"
+  "reattach-to-user-namespace"
+  "shellcheck"
+  "thefuck"
+  "tig"
+  "tmux"
+  "tree"
+  "vim"
+  "youtube-dl"
+)
+
+for package in "${packages[@]}"
+do
+  read -p "Install ${package} ? (y/n) " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    if [[ $machine = "Mac" ]]
+    then
+      echo "Installing ${package}"
+      eval $install_command $package
+    fi
+  fi
+done
+
+echo "Done"
